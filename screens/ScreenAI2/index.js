52cce427cb3d531cf732b1ee2a6055e51302513e
ravinder yadav <ravinder.y@crowdbotics.com>
2024-02-01 17:57:21 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI2/index.js
+++ screens/ScreenAI2/index.js
@@ -1,26 +1,29 @@
-import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, TextInput, Button, Image, Text } from 'react-native';
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, TextInput, Button, Image, Text, TouchableOpacity } from "react-native";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return <SafeAreaView style={styles.container}>
       <Image style={styles.logo} source={{
-      uri: 'https://tinyurl.com/42evm3m3'
+      uri: "https://tinyurl.com/42evm3m3"
     }} />
       <Text style={styles.title}>Welcome Back!</Text>
       <TextInput style={styles.input} onChangeText={setEmail} value={email} placeholder="Email" keyboardType="email-address" />
       <TextInput style={styles.input} onChangeText={setPassword} value={password} placeholder="Password" secureTextEntry />
       <Button title="Login" onPress={() => {}} />
+      <TouchableOpacity onPress={() => {}}>
+        <Text style={styles.forgotPassword}>Forgot Password?</Text>
+      </TouchableOpacity>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
   logo: {
     width: 100,
@@ -33,11 +36,15 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    width: '80%',
-    borderColor: 'gray',
+    width: "80%",
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 20,
     paddingLeft: 10
+  },
+  forgotPassword: {
+    marginTop: 15,
+    color: 'blue'
   }
 });
 export default LoginScreen;
